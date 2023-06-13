15dea3caed224b2ec94ed4a88753fabef12bccb2
JanZinch <jan.zinchanka@mail.ru>
2023-06-13 22:23:18 +0300

    feat: aiming

---
+++ Assets/Scripts/Controllers/ShootingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Controllers
+{
+    public class ShootingController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public Action OnStartAiming = null;
+        public Action OnEndAiming = null;
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            OnStartAiming?.Invoke();
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            OnEndAiming?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using Controllers;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Player
 {
@@ -7,26 +12,57 @@ namespace Player
         [SerializeField] private float _speed;
         
         [Space]
-        [SerializeField] private Joystick _joystick;
+        [SerializeField] private Joystick _motionJoystick;
+        [SerializeField] private ShootingController _shootingController;
         [SerializeField] private CharacterController _characterController;
         [SerializeField] private Animator _animator;
         
         private Vector3 _motion = default;
         private static readonly int SpeedParam = Animator.StringToHash("Speed");
 
+        private int _upperAvatarLayerIndex;
+
+        private void Awake()
+        {
+            _upperAvatarLayerIndex = _animator.GetLayerIndex("UpperAvatar");
+        }
+
+        private void OnEnable()
+        {
+            _shootingController.OnStartAiming += SetAiming;
+            _shootingController.OnEndAiming += RemoveAiming;
+        }
+
         private void FixedUpdate()
         {
-            _motion.Set(_joystick.Horizontal * _speed, 0.0f, _joystick.Vertical * _speed);
+            _motion.Set(_motionJoystick.Horizontal * _speed, 0.0f, _motionJoystick.Vertical * _speed);
             _characterController.Move(_motion);
             
             _animator.SetFloat(SpeedParam, Mathf.Max(Mathf.Abs(_motion.normalized.x), Mathf.Abs(_motion.normalized.z)));
-
-            //Vector3 vector = Vector3.up * _joystick.Horizontal + Vector3.left * _joystick.Vertical;
-
+            
             if (_motion != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(_motion.normalized, Vector3.up);
             }
         }
+
+        [EasyButtons.Button]
+        private void SetAiming()
+        {
+            _animator.SetLayerWeight(_upperAvatarLayerIndex, 1.0f);
+        }
+
+        [EasyButtons.Button]
+        private void RemoveAiming()
+        {
+            _animator.SetLayerWeight(_upperAvatarLayerIndex, 0.0f);
+        }
+
+        private void OnDisable()
+        {
+            _shootingController.OnStartAiming -= SetAiming;
+            _shootingController.OnEndAiming -= RemoveAiming;
+        }
+        
     }
 }
