6555104cbd5264a8ed5c4b0189782977b99b39ff
JanZinch <jan.zinchanka@mail.ru>
2023-06-13 16:19:43 +0300

    feat: joystick

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        
+        [Space]
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private Joystick _joystick;
+
+        private Vector3 _motion = default;
+
+        private void FixedUpdate()
+        {
+            _motion.Set(_joystick.Horizontal * _speed, 0.0f, _joystick.Vertical * _speed);
+            _characterController.Move(_motion);
+        }
+    }
+}
